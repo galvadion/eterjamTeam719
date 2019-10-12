abbab3017528011b07570f9a5541ab8ca7de509a
Bybase07 <francobarreto911@gmail.com>
2019-10-12 16:48:48 -0300

    Movimiento de Objetos y detector de colisiones
    El personaje detecta cuando hay un objeto movil y permite poder moverlo.
    Si hay un boton y el objeto movible esta encima, se activa, sino no.

---
+++ eterjamTeam719/Assets/ScriptsFranco/ObjectMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "FloorButton")
+        {
+            Debug.Log("Se abre una puerta...");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "FloorButton")
+        {
+            Debug.Log("Se cierra la puerta...");
+        }
+    }
+}
