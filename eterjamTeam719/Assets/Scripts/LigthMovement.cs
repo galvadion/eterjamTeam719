e15a1c130ec9f599905a94be2f833a269398bece
Bybase07 <francobarreto911@gmail.com>
2019-10-13 12:43:52 -0300

    Reduccion de angulo de la luz a 90 grados

---
+++ eterjamTeam719/Assets/Scripts/LigthMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigthMovement : MonoBehaviour
+{
+    public Transform player;
+    public float playerSpeed;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 lookAt = mouseScreenPosition;
+        float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
+        float AngleDeg = (45/ Mathf.PI) * AngleRad;
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+}
--- eterjamTeam719/Assets/Scripts/PlayerController.cs
+++ eterjamTeam719/Assets/Scripts/PlayerController.cs
@@ -20,12 +20,6 @@ public class PlayerController : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         transform.position += new Vector3((h * speed * Time.deltaTime), (v * speed * Time.deltaTime), 0f);
-        
-        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 lookAt = mouseScreenPosition;
-        float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
-        float AngleDeg = (180 / Mathf.PI) * AngleRad;
-        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
