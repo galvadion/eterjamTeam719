f4ec61715cec90a9ba2dfb9e4eba1a0f279d3721
Diego Becco <urashimasan@gmail.com>
2019-10-13 16:07:06 -0300

    ASDASDAD
    SADASDSA

--- eterjamTeam719/Assets/Scripts/LigthMovement.cs
+++ eterjamTeam719/Assets/Scripts/LigthMovement.cs
@@ -13,10 +13,10 @@ public class LigthMovement : MonoBehaviour
     }
     void Update()
     {
-        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+   /*     Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 lookAt = mouseScreenPosition;
         float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
-        float AngleDeg = (45/ Mathf.PI) * AngleRad;
-        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+        float AngleDeg = (180/ Mathf.PI) * AngleRad;
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);*/
     }
 }
--- eterjamTeam719/Assets/Scripts/PlayerController.cs
+++ eterjamTeam719/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header ("Movement")]
+    [Header("Movement")]
     public float speed;
     public Animator anim;
     public Vector2 movement;
+    public GameObject flashlight;
 
     private GameManager gM;
     private Rigidbody2D rb;
 
     private void Start()
     {
-      gM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gM = GameObject.Find("GameManager").GetComponent<GameManager>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,13 +24,29 @@ public class PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        anim.SetFloat("Horizontal" ,movement.x);
+        anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
+        if (movement.y == 1)
+        {
+            flashlight.transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else if (movement.y == -1)
+        {
+            flashlight.transform.rotation = Quaternion.Euler(0,0,180);
+        }
+        else if (movement.x == -1)
+        {
+            flashlight.transform.rotation = Quaternion.Euler(0,0,90);
+        }
+        else if (movement.x == 1)
+        {
+            flashlight.transform.rotation = Quaternion.Euler(0,0,270);  	
+        }
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 
